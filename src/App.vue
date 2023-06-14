666b783f11d61304c9ab0ed00941df7d6815b63e
hubinchang <hubc@startsi.cn>
2023-06-14 22:06:26 +0800

    alter

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <div> <h1>{{ '请根据要办理的业务进入对应的程序。' }}</h1></div>
+  <div style="display: flex;">
+    <HelloWorld name="程序1" icon="/src/assets/electron.svg" exe="F:\Fiddler\Fiddler.exe" />
+    <HelloWorld name="程序2" icon="/src/assets/vite.svg" exe="aaa" />
+    <HelloWorld name="程序3" icon="/src/assets/vue.svg" exe="aaa" />
+  </div>
+  
+</template>
+
+<style>
+.flex-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 6em;
+  width: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo.electron:hover {
+  filter: drop-shadow(0 0 2em #9FEAF9);
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ipcRenderer } from 'electron'
+defineProps<{ name: string, exe: string, icon: string }>()
+
+const run = (exe:string) => {
+  ipcRenderer.send('run', exe)
+}
+</script>
+
+<template>
+  <div class="app" @click="run(exe)">
+    <img :src="icon" class="logo electron" :alt="name" />
+    <span>{{ name }}</span>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
